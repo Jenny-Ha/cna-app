33412b2bdde8c8b22ec76f1a77a276fee873bc9b
Jenny-Ha <jennyha069@gmail.com>
2018-08-27 13:23:20 -0500

    #1 Fixed grid

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,24 +3,31 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import '../routes.dart';
 
 List<StaggeredTile> _staggeredTiles = const <StaggeredTile>[
-  // const StaggeredTile.count(column, row),
+  // const StaggeredTile.count(ancho, alto),
   const StaggeredTile.count(6, 5),
   const StaggeredTile.count(4, 5),
+
   const StaggeredTile.count(4, 5),
   const StaggeredTile.count(6, 5),
+
   const StaggeredTile.count(3, 5),
   const StaggeredTile.count(4, 5),
   const StaggeredTile.count(3, 5),
+
+  const StaggeredTile.count(6, 5),
+  const StaggeredTile.count(4, 5),
 ];
 
 List<Widget> _tiles = const <Widget>[
-  const _Example01Tile(Colors.green, Icons.widgets),
-  const _Example01Tile(Colors.lightBlue, Icons.wifi),
-  const _Example01Tile(Colors.amber, Icons.panorama_wide_angle),
-  const _Example01Tile(Colors.brown, Icons.map),
-  const _Example01Tile(Colors.deepOrange, Icons.send),
-  const _Example01Tile(Colors.indigo, Icons.airline_seat_flat),
-  const _Example01Tile(Colors.red, Icons.bluetooth),
+  const _ServiceTile(Colors.green, Icons.widgets, "TV en Vivo"),
+  const _ServiceTile(Colors.lightBlue, Icons.wifi, "Web Movil"),
+  const _ServiceTile(Colors.amber, Icons.panorama_wide_angle, "La Encuesta"),
+  const _ServiceTile(Colors.indigo, Icons.airline_seat_flat, "Radio en Vivo"),
+  const _ServiceTile(Colors.brown, Icons.map, "Teléfonos de Emergencia"),
+  const _ServiceTile(Colors.deepOrange, Icons.send, "Redes Sociales"),
+  const _ServiceTile(Colors.red, Icons.bluetooth, "Servicios"),
+  const _ServiceTile(Colors.green, Icons.widgets, "Periódicos"),
+  const _ServiceTile(Colors.lightBlue, Icons.wifi, "Radios"),
 ];
 
 class Home extends StatelessWidget {
@@ -28,23 +35,24 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
         body: new Padding(
-            padding: const EdgeInsets.only(top: 30.0),
+            padding: const EdgeInsets.only(top: 30.0), //TODO: save rea 
             child: new StaggeredGridView.count(
               crossAxisCount: 10,
               staggeredTiles: _staggeredTiles,
               children: _tiles,
-              mainAxisSpacing: 4.0,
-              crossAxisSpacing: 4.0,
+              mainAxisSpacing: 3.0,
+              crossAxisSpacing: 3.0,
               padding: const EdgeInsets.all(4.0),
             )));
   }
 }
 
-class _Example01Tile extends StatelessWidget {
-  const _Example01Tile(this.backgroundColor, this.iconData);
-
+class _ServiceTile extends StatelessWidget {
   final Color backgroundColor;
   final IconData iconData;
+  final String title;
+
+  const _ServiceTile(this.backgroundColor, this.iconData, this.title);
 
   @override
   Widget build(BuildContext context) {
@@ -52,16 +60,28 @@ class _Example01Tile extends StatelessWidget {
       color: backgroundColor,
       child: new InkWell(
         onTap: () {},
-        child: new Center(
-          child: new Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: new Icon(
-              iconData,
-              color: Colors.white,
-            ),
+        child: new Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: new Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Icon(
+                iconData,
+                color: Colors.white,
+              ),
+              new Text(
+                title,
+                textAlign: TextAlign.center,
+                style: new TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
           ),
         ),
       ),
-    );
+    ); 
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:cna/main.dart';
+import 'package:CNA/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
