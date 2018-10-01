ff7fd4e50b06d6ea449bd056980ad64f63dc0a7e
luisaGonzales <luisagc711@gmail.com>
2018-10-01 14:40:52 -0500

    #8 advance of newspaper list

--- lib/screens/newspapers.dart
+++ lib/screens/newspapers.dart
@@ -1,21 +1,65 @@
+import 'package:CNA/models/newspaper.dart';
 import 'package:flutter/material.dart';
-import '../routes.dart';
+import '../utils/NewIcons.dart';
+
 
 class Newspapers extends StatelessWidget {
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         title: Text('Periódicos'),
-      ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Newspapers screen'),
-          onPressed: () {
-            // Navigate to Periódicos when tapped!
-          },
-        ),
+      ),*/
+      body: Column(
+          children: <Widget>[
+            new Expanded(
+              child: new SafeArea(
+                child: new CustomScrollView(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: false,
+                  slivers: <Widget>[
+                    new SliverPadding(
+                      padding: const EdgeInsets.symmetric(vertical: 0.0),
+                      sliver: new SliverList(
+                          delegate: new SliverChildBuilderDelegate(
+                                (context, index) => new NewspaperRow(newspapers[index]),
+                            childCount: newspapers.length,
+                          ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
       ),
     );
   }
 }
+
+class NewspaperRow extends StatelessWidget {
+
+  final Newspaper newspaper;
+
+  NewspaperRow(this.newspaper);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
+      height: 210.0,
+      color: this.newspaper.background,
+      child: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Image.asset(
+            this.newspaper.image,
+            width: 160.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
